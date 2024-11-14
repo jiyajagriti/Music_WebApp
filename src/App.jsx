01cff3a1ab30e26b5d388b9f09fda6b79f012880
Jiya Jagriti <jiya.jagriti2004@.com>
2024-11-14 23:03:10 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <ul className='flex justify-around'>
+          <li>Home</li>
+          <li>About</li>
+          <li>ContacT Us</li>
+        </ul>
+    </div>
+  )
+}
+
+export default App
